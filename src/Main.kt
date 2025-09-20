c28229ca2203016b8477cd9b53e81c4f3a57b045
Daniil <dl9morozova@gmail.com>
2025-09-20 12:16:59 +0300

    changes

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,19 @@
 fun main() {
+    print("Введите первое число: ")
     val num = readln().toInt()
-    println(number)
-    println()
-    print()
+    print("Введите второе число: ")
+    val num2 = readln().toInt()
+    if (num > num2) {
+        println("Первое число больше")
+    } else if (num == num2) {
+        println("Числы равны")
+    } else {
+        println("Второе число больше")
+    }
+    val max = if (num > num2) num else num2
+    println("Максимальное число: $max")
+    val age = readln().toInt()
+    val status = if (age >= 18) "Взрослый" else "Ребенок"
+    println(status)
+
 }
